e585ac99650b8b0ff81785fb86816ce65dfefd26
Daniel Lewis <dewis@sympletech.com>
2014-02-16 17:16:31 -0800

    Added Admin Project

---
+++ GeoTour.Admin/js/geo-tour-admin.js
@@ -0,0 +1,19 @@
+﻿var geoTourAdmin = new (function () {
+    var self = this;
+
+    self.map = null;
+
+    self.init = function() {
+        navigator.geolocation.getCurrentPosition(function(location) {
+
+            require(["esri/map"], function (Map) {
+                self.map = new Map("map-surface", {
+                    basemap: 'streets',
+                    zoom: 10,
+                    center: [location.coords.longitude, location.coords.latitude],
+                });
+            });
+        });        
+    };
+    self.init();
+})();
